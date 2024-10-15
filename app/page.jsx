1a942a18bc15bcfbd6030efdd871c84e8b94b9d7
Shivangi889 <cseshivangi599@gmail.com>
2024-10-15 14:50:00 +0530

    demo design

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import Home1 from "./(homes)/home-1/page";
+export const metadata = {
+  title: "Expert Engineers",
+  description: "Industry - Factory & Industrial React Nextjs Template",
+};
+
+export default function Home() {
+  return (
+    <>
+      <Home1 />
+    </>
+  );
+}
---
+++ data/menu.js
@@ -0,0 +1,438 @@
+export const homeItems = [
+  {
+    href: "/home-20",
+    src: "/demo/pic20.jpg",
+    colorTheme: "skin-1-theme",
+    label: "Home - Industry",
+  },
+  {
+    href: "/home-19",
+    src: "/demo/pic19.jpg",
+    colorTheme: "skin-2-theme",
+    label: "Home - Industry",
+  },
+  {
+    href: "/home-18",
+    src: "/demo/pic18.jpg",
+    colorTheme: "skin-2-theme",
+    label: "Home - Industry",
+  },
+  {
+    href: "/home-17",
+    src: "/demo/pic17.jpg",
+    colorTheme: "skin-1-theme",
+    label: "Home - Industry",
+  },
+  {
+    href: "/",
+    src: "/demo/pic1.jpg",
+    colorTheme: "skin-1-theme",
+    label: "Home - Default",
+  },
+  {
+    href: "/home-2",
+    src: "/demo/pic2.jpg",
+    colorTheme: "skin-2-theme",
+    label: "Home - Oil/Gas Plant",
+  },
+  {
+    href: "/home-3",
+    src: "/demo/pic3.jpg",
+    colorTheme: "skin-2-theme",
+    label: "Home - Steel Plant",
+  },
+  {
+    href: "/home-4",
+    src: "/demo/pic4.jpg",
+    colorTheme: "skin-2-theme",
+    label: "Home - Factory",
+  },
+  {
+    href: "/home-5",
+    src: "/demo/pic5.jpg",
+    colorTheme: "skin-3-theme",
+    label: "Home - Construct",
+  },
+  {
+    href: "/home-6",
+    src: "/demo/pic6.jpg",
+    colorTheme: "skin-4-theme",
+    label: "Home - Solar Plant",
+  },
+  {
+    href: "/home-7",
+    src: "/demo/pic7.jpg",
+    colorTheme: "skin-5-theme",
+    label: "Home - Food Industry",
+  },
+  {
+    href: "/home-8",
+    src: "/demo/pic8.jpg",
+    colorTheme: "skin-6-theme",
+    label: "Home - Agriculture",
+  },
+  {
+    href: "/home-9",
+    src: "/demo/pic9.jpg",
+    colorTheme: "skin-7-theme",
+    label: "Home - Ship Industry",
+  },
+  {
+    href: "/home-10",
+    src: "/demo/pic10.jpg",
+    colorTheme: "skin-8-theme",
+    label: "Home - Leather Industry",
+  },
+  {
+    href: "/home-11",
+    src: "/demo/pic11.jpg",
+    colorTheme: "skin-2-theme",
+    label: "Home - Nuclear Plant",
+  },
+  {
+    href: "/home-12",
+    src: "/demo/pic12.jpg",
+    colorTheme: "skin-9-theme",
+    label: "Home - Beer Factory",
+  },
+  {
+    href: "/home-13",
+    src: "/demo/pic13.jpg",
+    colorTheme: "skin-2-theme",
+    label: "Home - Mining Industry",
+  },
+  {
+    href: "/home-14",
+    src: "/demo/pic14.jpg",
+    colorTheme: "skin-2-theme",
+    label: "Home - Car Industry",
+  },
+  {
+    href: "/home-15",
+    src: "/demo/pic15.jpg",
+    colorTheme: "skin-7-theme",
+    label: "Home - Plastic Industry",
+  },
+  {
+    href: "/home-16",
+    src: "/demo/pic16.jpg",
+    colorTheme: "skin-10-theme",
+    label: "Home - Event Industry",
+  },
+];
+
+export const pagesItems = [
+  {
+    title: "LV/Mv Panels Manufacturing",
+    links: [
+      { href: "/about-1", label: "LV Panels" },
+      { href: "/about-2", label: "MV Panels" },
+      { href: "/services-1", label: " Bus Bar Trunking" },
+      { href: "/services-2", label: "Compact Sub Stations" },
+      // { href: "/services-details/Solar Panels", label: "Services Details" },
+    ],
+  },
+  {
+    title: "Trunking Projects",
+    links: [
+      { href: "/team-1", label: "Electrical" },
+      { href: "/team-2", label: "Mechanical" },
+      // { href: "/faq-1", label: "Faqs 1" },
+      // { href: "/faq-2", label: "Faqs 2" },
+      // { href: "/portfolio-grid-2", label: "Portfolio Grid 2" },
+    ],
+  },
+  {
+    title: " Automation Solutions",
+    links: [
+      { href: "/portfolio-grid-3", label: "Load Mangements System" },
+      { href: "/portfolio-grid-4", label: " Scada System" },
+      {
+        href: "/portfolio-details/Leather Products",
+        label: "Energy Management System(EMS)",
+      },
+      { href: "/error-403", label: "Building Management System (BMS)" },
+      { href: "/error-404", label: "Special Purpose Machines Automation" },
+      { href: "/error-404", label: "FRTU and Modems" },
+    ],
+  },
+  {
+    title: " Water Solutions",
+    links: [
+      { href: "/portfolio-grid-3", label: "Water Works" },
+      { href: "/portfolio-grid-4", label: " WasteWater" },
+      {
+        href: "/portfolio-details/Leather Products",
+        label: "Irrigation",
+      },
+      { href: "/error-403", label: "Borewell Automation System" },
+      { href: "/error-404", label: "Pumps" },
+      { href: "/error-404", label: "Valves" },
+      { href: "/error-404", label: "Filtration Vessels" },
+      { href: "/error-404", label: "Smart Valves" },
+      { href: "/error-404", label: "Flow Meters" },
+    ],
+  },
+  {
+    title: "Services",
+    links: [
+      { href: "/error-405", label: "Retrofitting Works" },
+      { href: "/coming-soon-1", label: "Audits" },
+      { href: "/coming-soon-2", label: "Testing" },
+      { href: "/help-desk", label: "AMC" },
+      { href: "/privacy-policy", label: "MISC Services" },
+    ],
+  },
+];
+
+export const blogItems = [
+  {
+    title: "Blog",
+    links: [
+      { href: "/blog-half-img", label: "Half image" },
+      { href: "/blog-half-img-sidebar", label: "Half image sidebar" },
+      { href: "/blog-half-img-left-sidebar", label: "Half image sidebar left" },
+      { href: "/blog-large-img", label: "Large image" },
+    ],
+  },
+  {
+    title: "Blog",
+    links: [
+      { href: "/blog-large-img-sidebar", label: "Large image sidebar" },
+      {
+        href: "/blog-large-img-left-sidebar",
+        label: "Large image sidebar left",
+      },
+      { href: "/blog-grid-2", label: "Grid 2" },
+      { href: "/blog-grid-2-sidebar", label: "Grid 2 sidebar" },
+    ],
+  },
+  {
+    title: "Blog",
+    links: [
+      { href: "/blog-grid-2-sidebar-left", label: "Grid 2 sidebar left" },
+      { href: "/blog-grid-3", label: "Grid 3" },
+      { href: "/blog-grid-3-sidebar", label: "Grid 3 sidebar" },
+      { href: "/blog-grid-3-sidebar-left", label: "Grid 3 sidebar left" },
+    ],
+  },                                                             
+  {
+    title: "Blog",
+    links: [
+      { href: "/blog-grid-4", label: "Grid 4" },
+      { href: "/blog-single/Seven Outrageous Ideas Industry", label: "Single" },
+      {
+        href: "/blog-single-sidebar/Seven Outrageous Ideas Industry",
+        label: "Single sidebar",
+      },
+      {
+        href: "/blog-single-left-sidebar/Seven Outrageous Ideas Industry",
+        label: "Single sidebar left",
+      },
+    ],
+  },
+];
+
+export const elementItems = [
+  {
+    title: "Element",
+    links: [
+      { href: "/shortcode-buttons", iconClass: "ti-mouse", label: "Buttons" },
+      {
+        href: "/shortcode-icon-box-styles",
+        iconClass: "ti--grid2",
+        label: "Icon box styles",
+      },
+      {
+        href: "/shortcode-pricing-table",
+        iconClass: "ti-layout-grid2-thumb",
+        label: "Pricing table",
+      },
+      {
+        href: "/shortcode-toggles",
+        iconClass: "ti-layout-accordion-separated",
+        label: "Toggles",
+      },
+      { href: "/shortcode-team", iconClass: "ti-user", label: "Team" },
+      {
+        href: "/shortcode-animation-effects",
+        iconClass: "ti-layers-alt",
+        label: "Animation Effects",
+      },
+    ],
+  },
+  {
+    title: "Element",
+    links: [
+      {
+        href: "/shortcode-carousel-sliders",
+        iconClass: "ti-layout-slider",
+        label: "Carousel sliders",
+      },
+      {
+        href: "/shortcode-image-box-content",
+        iconClass: "ti-image",
+        label: "Image box content",
+      },
+      {
+        href: "/shortcode-tabs",
+        iconClass: "ti-layout-tab-window",
+        label: "Tabs",
+      },
+      { href: "/shortcode-counters", iconClass: "ti-timer", label: "Counters" },
+      {
+        href: "/shortcode-shop-widgets",
+        iconClass: "ti-shopping-cart",
+        label: "Shop Widgets",
+      },
+      {
+        href: "/shortcode-filters",
+        iconClass: "ti-check-box",
+        label: "Gallery Filters",
+      },
+    ],
+  },
+  {
+    title: "Element",
+    links: [
+      {
+        href: "/shortcode-accordians",
+        iconClass: "ti-layout-accordion-list",
+        label: "Accordians",
+      },
+      {
+        href: "/shortcode-dividers",
+        iconClass: "ti-layout-list-post",
+        label: "Dividers",
+      },
+      {
+        href: "/shortcode-images-effects",
+        iconClass: "ti-layout-media-overlay",
+        label: "Images effects",
+      },
+      {
+        href: "/shortcode-testimonials",
+        iconClass: "ti-comments",
+        label: "Testimonials",
+      },
+      {
+        href: "/shortcode-pagination",
+        iconClass: "ti-more",
+        label: "Pagination",
+      },
+      {
+        href: "/shortcode-alert-box",
+        iconClass: "ti-alert",
+        label: "Alert box",
+      },
+    ],
+  },
+  {
+    title: "Element",
+    links: [
+      {
+        href: "/shortcode-icon-box",
+        iconClass: "ti-layout-media-left-alt",
+        label: "Icon Box",
+      },
+      {
+        href: "/shortcode-list-group",
+        iconClass: "ti-list",
+        label: "List group",
+      },
+      {
+        href: "/shortcode-title-separators",
+        iconClass: "ti-layout-line-solid",
+        label: "Title Separators",
+      },
+      {
+        href: "/shortcode-all-widgets",
+        iconClass: "ti-widgetized",
+        label: "Widgets",
+      },
+      {
+        href: "/shortcode-carousel-sliders",
+        iconClass: "ti-layout-slider",
+        label: "Carousel sliders",
+      },
+      {
+        href: "/shortcode-image-box-content",
+        iconClass: "ti-image",
+        label: "Image box content",
+      },
+    ],
+  },
+];
+
+export const contactLinks = [
+  { href: "/contact-1", label: "Contact us 1" },
+  { href: "/contact-2", label: "Contact us 2" },
+  { href: "/contact-3", label: "Contact us 3" },
+  { href: "/contact-4", label: "Contact us 4" },
+];
+
+export const shopLinks = [
+  { href: "/shop", label: "Shop" },
+  { href: "/shop-sidebar", label: "Shop Sidebar" },
+  { href: "/shop-product-details/Slim Fit Chinos", label: "Product Details" },
+  { href: "/shop-cart", label: "Cart" },
+  { href: "/shop-wishlist", label: "Wishlist" },
+  { href: "/shop-checkout", label: "Checkout" },
+  { href: "/shop-login", label: "Login" },
+  { href: "/shop-register", label: "Register" },
+];
+
+export const headerAndFooters = [
+  {
+    title: "Header Light",
+    iconClass: "fas fa-angle-right",
+    links: [
+      { href: "/header-style-1", label: "Header 1" },
+      { href: "/header-style-2", label: "Header 2" },
+      { href: "/header-style-3", label: "Header 3" },
+      { href: "/header-style-4", label: "Header 4" },
+      { href: "/header-style-5", label: "Header 5" },
+      { href: "/header-style-6", label: "Header 6" },
+    ],
+  },
+  {
+    title: "Header Dark",
+    iconClass: "fas fa-angle-right",
+    links: [
+      { href: "/header-style-dark-1", label: "Header 1" },
+      { href: "/header-style-dark-2", label: "Header 2" },
+      { href: "/header-style-dark-3", label: "Header 3" },
+      { href: "/header-style-dark-4", label: "Header 4" },
+      { href: "/header-style-dark-5", label: "Header 5" },
+      { href: "/header-style-dark-6", label: "Header 6" },
+    ],
+  },
+  {
+    title: "Footer",
+    iconClass: "fas fa-angle-right",
+    links: [
+      { href: "/footer-1", label: "Footer 1" },
+      { href: "/footer-2", label: "Footer 2" },
+      { href: "/footer-3", label: "Footer 3" },
+      { href: "/footer-4", label: "Footer 4" },
+      { href: "/footer-5", label: "Footer 5" },
+      { href: "/footer-6", label: "Footer 6" },
+      { href: "/footer-7", label: "Footer 7" },
+      { href: "/footer-8", label: "Footer 8" },
+      { href: "/footer-9", label: "Footer 9" },
+      { href: "/footer-10", label: "Footer 10" },
+      { href: "/footer-11", label: "Footer 11" },
+      { href: "/footer-12", label: "Footer 12" },
+    ],
+  },
+];
+export const onepageNavItems = [
+  { href: "#home", text: "Home" },
+  { href: "#about", text: "About Us" },
+  { href: "#choose-us", text: "Why Choose Us" },
+  { href: "#services", text: "Our Services" },
+  { href: "#latestnews", text: "Latest News" },
+  { href: "#team", text: "Our Team" },
+  { href: "#client", text: "Our Client" },
+  { href: "#footer", text: "Contact Us" },
+];
